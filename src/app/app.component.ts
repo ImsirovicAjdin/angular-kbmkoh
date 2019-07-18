9c54f0e648eac2b628f05514c5655bdbbf12c274
ImsirovicAjdin <ajdika@live.com>
2019-07-18 10:02:58 +0200

    Assign an array of objects to the vehicles property inside AppComponent class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  vehicles = [
+    {
+      id: 1,
+      make: 'Toyota',
+      color: {
+        body: 'red',
+        interior: 'gray'
+      }
+    },
+    {
+      id: 2,
+      make: 'Subaru',
+      color: {
+        body: 'red',
+        interior: 'gray'
+      }
+    },
+    {
+      id: 3, 
+      make: 'Subaru',
+      old: "yes"
+    },
+    {
+      id: 4,
+      make: 'Subaru',
+      color: {
+        body: 'yellow',
+        interior: 'gray'
+      }
+    }
+  ]
 }
