24ef5b86a285bbe1c7efaa1a3787fc6f1862d3cc
ImsirovicAjdin <ajdika@live.com>
2019-07-18 09:53:34 +0200

    (1) Delete hello component, app.component.css, app.component.html, (2) remove hello component import from app.module.ts, (3) add in-file template to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  template: `
+    <div>
+      Parent Component
+    </div>
+  `
 })
 export class AppComponent  {
   name = 'Angular';
